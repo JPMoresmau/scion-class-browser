a5575e067fc6b05ac19c979b1aa07be96e77faa3
jpmoresmau <jp@moresmau.fr>
2013-06-03 23:23:02 +0200

    option to dump uncompressed response

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
@@ -19,32 +19,39 @@ import Scion.PersistentBrowser.Util (logToStdout)
 
 import GHC.IO.Handle (hDuplicate,hDuplicateTo)
 
+
 main :: IO ()
 main = do args <- getArgs 
           case args of
-            ("--version":_) -> putStrLn ("scion-browser executable, version " ++ (showVersion version))
-            _               -> do runStateT (runInputT defaultSettings loop) initialState
-                                  return ()
+            ("--version":_) -> putStrLn ("scion-browser executable, version " ++ showVersion version)
+            ("--clear":_)     -> run False
+            _               -> run True
+            
+run :: Bool -> IO()       
+run doCompression=do
+        runStateT (runInputT defaultSettings (loop doCompression)) initialState
+        return ()
 
-loop :: InputT BrowserM ()
-loop = do 
+loop :: Bool -> InputT BrowserM ()
+loop doCompression = do 
           maybeLine <- getInputLine ""
           case maybeLine of
             Nothing -> return () -- ctrl+D or EOF
-            Just line -> do
-              case Atto.parse json (BSU.fromString line) of
-                Atto.Fail _ _ e   -> (liftIO $ logToStdout ("error in command: " ++ e)) >> loop
-                Atto.Partial _   -> (liftIO $ logToStdout ("incomplete data error in command: ")) >> loop
-                Atto.Done _ value -> case T.parse parseJSON value of
-                                       Error e     -> (liftIO $ logToStdout ("error in command: " ++ e)) >> loop
+            Just line -> 
+              case Atto.parseOnly json (BSU.fromString line) of
+                Left e   -> liftIO (logToStdout ("error in command: " ++ e)) >> loop doCompression
+                Right value -> case T.parse parseJSON value of
+                                       Error e     -> liftIO (logToStdout ("error in command: " ++ e)) >> loop doCompression
                                        Success cmd -> do 
                                                          stdout_excl <- liftIO $ hDuplicate stdout
                                                          liftIO $ hDuplicateTo stderr stdout  -- redirect stdout to stderr
                                                          (res, continue) <- lift $ executeCommand cmd
                                                          liftIO $ hDuplicateTo stdout_excl stdout  -- redirect stdout to original stdout
                                                          let encoded    = LBS.append (encode res) "\n"
-                                                             compressed = Zlib.compressWith Zlib.defaultCompressParams { Zlib.compressLevel = Zlib.bestSpeed } encoded
+                                                             compressed = if doCompression 
+                                                                then Zlib.compressWith Zlib.defaultCompressParams { Zlib.compressLevel = Zlib.bestSpeed } encoded
+                                                                else encoded
                                                          liftIO $ LBS.putStr compressed
                                                          liftIO $ hFlush stdout
-                                                         if continue then loop else return ()
+                                                         when continue $ loop doCompression
 
